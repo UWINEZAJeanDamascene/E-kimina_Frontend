ff242b567a1602e2d26f626ec35082c6335268ec
Shema Arsene <aniyonkurushema@gmail.com>
2025-10-17 13:52:04 +0200

    Project initialization

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Welcome to E-Kimina!</h1>
+      <p className="mt-4 text-lg text-center">
+        This is the home page of the E-Kimina application.
+      </p>
+    </div>
+  )
+}
+
+export default Home
